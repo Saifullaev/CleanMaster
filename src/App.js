01385827b347675ec1d224ecdc2a4fbccb191243
Saifullaev <saifullaevkuanysh@gmail.com>
2023-05-17 16:51:36 +0600

    первый

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все что надо знать о химчистке</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
