5911cd24b28062702c57f48349c26b1550617f9a
Saifullaev <saifullaevkuanysh@gmail.com>
2023-05-25 16:18:29 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import './scss/app.scss';
-
+import './css/App.css';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
+import Container from './components/Container';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div>
       <Header />
-      <h1>TEST</h1>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все что надо знать о химчистке</h2>
-        </div>
-      </div>
+      <Container />
     </div>
   );
 }
