8f7d7273cd91d5651d98091edfc46e448049a354
Saifullaev <saifullaevkuanysh@gmail.com>
2023-05-28 00:08:19 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import './scss/app.scss';
-
+import './css/App.css';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
+import Container from './components/Container';
+import Kartinatext from './components/Kartinatext';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div>
       <Header />
-      <h1>TEST</h1>
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все что надо знать о химчистке</h2>
-        </div>
-      </div>
+      <Container />
+      <Kartinatext />
     </div>
   );
 }
