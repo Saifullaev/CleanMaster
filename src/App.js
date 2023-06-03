e5e4b4e1580b9c5c28c559768fded6e9f190b1f8
Saifullaev <saifullaevkuanysh@gmail.com>
2023-06-03 15:20:53 +0600

    price

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import './css/App.css';
 import Container from './components/container';
 import Header from './components/header';
+import PriceBlok from './components/firstBlock/PriceBlok';
 
 function App() {
   return (
     <div>
       <Header />
       <Container />
-      {/* <Kartinatext /> */}
+      <div>
+        <h1>Наши цены по химчистке:</h1>
+        <PriceBlok
+          image={require('../src/components/firstBlock/compon/divan uglovoi.jpg')}
+          price="7000 тенге."
+        />
+        <PriceBlok
+          image={require('../src/components/firstBlock/compon/divan odin.jpg')}
+          price="5000 тенге."
+        />
+        <PriceBlok
+          image={require('../src/components/firstBlock/compon/kreslo.jpg')}
+          price="3000 тенге."
+        />
+        <PriceBlok
+          image={require('../src/components/firstBlock/compon/pufic.jpg')}
+          price="1000 тенге."
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/firstBlock/PriceBlok.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './PriceBlok.css';
+
+const PriceBlok = ({ image, price }) => {
+  return (
+    <div className="price-blok">
+      <div className="image">
+        <img src={image} alt="Product" />
+      </div>
+      <div className="text">
+        <h2>Цена: {price}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default PriceBlok;
