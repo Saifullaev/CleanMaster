f2e52a93eb7b848a16409d38118bb11745240ec2
Saifullaev <saifullaevkuanysh@gmail.com>
2023-06-04 22:33:09 +0600

    slider

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import './css/App.css';
 import Container from './components/container';
 import Header from './components/header';
 import PriceBlok from './components/firstBlock/PriceBlok';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import divan from './components/firstBlock/compon/divan odin.jpg';
+import divanSecond from './components/firstBlock/compon/divan uglovoi.jpg';
+import divanThirt from './components/firstBlock/compon/kreslo.jpg';
+import divanFourth from './components/firstBlock/compon/pufic.jpg';
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div>
       <Header />
       <Container />
       <div>
         <h1>Наши цены по химчистке:</h1>
-        <PriceBlok
-          image={require('../src/components/firstBlock/compon/divan uglovoi.jpg')}
-          price="7000 тенге."
-        />
-        <PriceBlok
-          image={require('../src/components/firstBlock/compon/divan odin.jpg')}
-          price="5000 тенге."
-        />
-        <PriceBlok
-          image={require('../src/components/firstBlock/compon/kreslo.jpg')}
-          price="3000 тенге."
-        />
-        <PriceBlok
-          image={require('../src/components/firstBlock/compon/pufic.jpg')}
-          price="1000 тенге."
-        />
+        <Slider {...settings}>
+          <PriceBlok image={divan} price="5000 тенге." />
+          <PriceBlok image={divanSecond} price="5000 тенге." />
+          <PriceBlok image={divanThirt} price="3000 тенге." />
+          <PriceBlok image={divanFourth} price="1000 тенге." />
+        </Slider>
       </div>
     </div>
   );
