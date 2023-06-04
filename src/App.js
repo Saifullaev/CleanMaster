ed3129065d5b9b482415050908aa8607d90adc85
Kassymzhan Zhandos <zandoskasenov@gmail.com>
2023-06-04 16:15:16 +0600

    carta

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './css/App.css';
 import Container from './components/container';
 import Header from './components/header';
+import Info from './components/info/Info';
+import Carta from './components/carta/Carta';
 
 function App() {
   return (
     <div>
       <Header />
       <Container />
+      <Info/>
+      <Carta/>
       {/* <Kartinatext /> */}
     </div>
   );
